042e2caf974e0bc5da812b1efa43abcf42cc5247
Davidetat1 <davidetatone92@gmail.com>
2024-05-16 19:59:15 +0200

    Esercizio 7 Testing Base dati

---
+++ esercizio-h2-mock/src/main/java/co/develhope/esercizioh2mock/EsercizioH2MockApplication.java
@@ -0,0 +1,13 @@
+package co.develhope.esercizioh2mock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsercizioH2MockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsercizioH2MockApplication.class, args);
+	}
+
+}
