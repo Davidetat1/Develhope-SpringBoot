53aec4230b3cdc1fe140ba614b1c75eab2389663
Davidetat1 <davidetatone92@gmail.com>
2024-06-23 18:22:48 +0200

    Esercizio 22 Spring Boot - Upload & Download

---
+++ esercizio-file-upload-and-download/src/main/java/co/develhope/esercizio_file_upload_and_download/EsercizioFileUploadAndDownloadApplication.java
@@ -0,0 +1,13 @@
+package co.develhope.esercizio_file_upload_and_download;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsercizioFileUploadAndDownloadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsercizioFileUploadAndDownloadApplication.class, args);
+	}
+
+}
---
+++ esercizio-file-upload-and-download/src/test/java/co/develhope/esercizio_file_upload_and_download/EsercizioFileUploadAndDownloadApplicationTests.java
@@ -0,0 +1,13 @@
+package co.develhope.esercizio_file_upload_and_download;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsercizioFileUploadAndDownloadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
